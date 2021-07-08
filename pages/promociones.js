731a6c214cdd74ae392069c99e487a65b3965ef9
Joel <86894648+joelclementes@users.noreply.github.com>
2021-07-07 19:41:03 -0500

    agregué las propociones y cambié logo del encabezado2

--- pages/promociones.js
+++ pages/promociones.js
@@ -3,7 +3,7 @@ import Layout from "../components/Layout";
 const Promociones = () => (
     <Layout>
         <div className="text-center pb-4">
-            <img src="image/promos.png"></img>
+            <img src="/image/promos.png"></img>
         </div>
     </Layout>
 )
