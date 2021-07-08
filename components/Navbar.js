14101cf873ed38126a9940ff48fcbf78cc8395ad
Joel <86894648+joelclementes@users.noreply.github.com>
2021-07-07 19:42:43 -0500

    cambié menu en navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -9,9 +9,9 @@ const Navbar = () => (
                 <a className="navbar-brand" >Chato's Hamburguesas</a>
             </Link>
             <LinkPedido/>
-            {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
-            </button> */}
+            </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav  ms-auto mb-2 mb-lg-0">
                     <li className="nav-item">
